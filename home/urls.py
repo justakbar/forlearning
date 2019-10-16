4785478c8306baa692cb77ae1103156705722b69
Akbar <akbarhacker9899@gmail.com>
2019-10-16 21:58:56 +0300

    added home app

--- home/urls.py
+++ home/urls.py
@@ -14,9 +14,13 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include, re_path
+from django.urls import path
+from django.http import HttpResponse
+
+
+def index(request):
+    return HttpResponse("Hello, world. You're at the polls index.")
 
 urlpatterns = [
-	path('', include('home.urls')),
-    path('admin/', admin.site.urls),
+	path('', index)
 ]
